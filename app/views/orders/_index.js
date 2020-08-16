ad99a9a6438a857467ed05fa138e7600e4c95152
jurogrammer <solution_12@kuh.ac.kr>
2020-08-16 18:02:07 +0900

    Add Click-Event in modal att

--- app/views/orders/_index.js
+++ app/views/orders/_index.js
@@ -29,7 +29,7 @@ var ordersTable = $('#orders').DataTable( {
     {
       data: "orderNumber",
       render: function (data, type, row) {
-        return "<a href=\"#orderdetail\" rel=\"modal:open\">" + data + "</a>"
+        return "<a href=\"#orderdetail\" rel=\"modal:open\" onclick = showOrderNumber(" + data + ")>" + data + "</a>"
     }},
     {data: "orderDate"},
     {data: "requiredDate"},
@@ -39,7 +39,7 @@ var ordersTable = $('#orders').DataTable( {
     {
       data: "customerNumber",
       render: function (data, type, row) {
-        return "<a href=\"#customer\" rel=\"modal:open\">" + data + "</a>"
+        return "<a href=\"#customer\" rel=\"modal:open\" onclick = showCustomerInfo(" + data + ")>" + data + "</a>"
       }
     }
   ]
